f2d3f7cf264fc49aa5696d385fedc79985270e2c
jcleow <>
2021-12-07 14:14:46 +0800

    add some buttons

--- src/components/V3DApp.js
+++ src/components/V3DApp.js
@@ -1,8 +1,19 @@
 import React from "react";
-
+import Button from "react-bootstrap/Button";
 import * as v3dAppAPI from "../v3dApp/app.js";
 import "../v3dApp/app.css";
 
+const Header = () => {
+  return (
+    <div className="sidebar">
+      <div class="d-flex flex-column">
+        <Button className="p-2 w-50">Show Annotation </Button>
+        <Button className="p-2 w-50">Hide Annotation </Button>
+        <Button className="p-2 w-50">Show more Details </Button>
+      </div>
+    </div>
+  );
+};
 class V3DApp extends React.Component {
   #app = null;
 
@@ -27,6 +38,7 @@ class V3DApp extends React.Component {
           className="fullscreen-button fullscreen-open"
           title="Toggle fullscreen mode"
         ></div>
+        <Header />
       </div>
     );
   }
